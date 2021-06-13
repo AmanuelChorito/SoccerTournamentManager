b258056d8b9f2941908470788a88b9acd804a24b
AmanuelChorito <amye.end@gmail.com>
2021-06-13 17:19:56 -0500

    Third Commit

--- db_connection.js
+++ db_connection.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql");
-var conn = mysql.createConnection({
+var conn = mysql.createPool({
   host: "us-cdbr-east-04.cleardb.com",
   user: "b0f3977a8a737c",
   password: "7c66c3c3",
   database: "heroku_082c330c1b339b6",
 });
-conn.connect(function (err) {
+/* conn.connect(function (err) {
   if (err) throw err;
   console.log("conneced");
-});
+}); */
 module.exports = conn;
